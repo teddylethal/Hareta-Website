45e0dacb7a785a9bcb432d6f932e6442c26fc0c7
teddylethal <teddy.lethal@gmail.com>
2023-07-24 17:09:23 +0700

    finish install font awesome

--- src/pages/ProductList/Product/Product.tsx
+++ src/pages/ProductList/Product/Product.tsx
@@ -1,4 +1,5 @@
-import Button from 'src/components/Button'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartPlus, faHeart } from '@fortawesome/free-solid-svg-icons'
 
 export default function Product() {
   return (
@@ -14,15 +15,17 @@ export default function Product() {
           <button className='text-xs text-gray-500 hover:text-haretaColor'>Porsche collection</button>
         </div>
         <button>
-          <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor' className='h-6 w-6'>
-            <path d='M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z' />
-          </svg>
+          <FontAwesomeIcon icon={faHeart} fontSize={24} />
         </button>
       </div>
       <div className='mx-2 my-1 flex items-center justify-between'>
         <span className='text-haretaColor'>$30</span>
-        <button className='rounded-md bg-[#ccc] px-2 py-1 text-sm text-textDark hover:bg-haretaColor dark:bg-[#111] dark:text-textLight dark:hover:bg-haretaColor'>
-          Add to cart
+        <button className=''>
+          <FontAwesomeIcon
+            icon={faCartPlus}
+            fontSize={24}
+            className='text-textDark hover:text-haretaColor dark:text-textLight dark:hover:text-haretaColor'
+          />
         </button>
       </div>
     </div>
