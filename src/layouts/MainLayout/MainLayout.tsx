411bffd4ed0a01c7c4030cabbc9376a5fd77b38b
trongvan245 <trongvan159357@gmail.com>
2023-08-30 21:13:37 +0700

    ...

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,10 +1,10 @@
-import { AnimatePresence, motion } from 'framer-motion'
-import { Fragment, useEffect, useState } from 'react'
+// import { AnimatePresence, motion } from 'framer-motion'
+// import { Fragment, useEffect, useState } from 'react'
 import Footer from 'src/components/Footer'
 import Header from 'src/components/Header'
-import { useScrollDirection } from 'src/hooks/useScrollDirection'
+// import { useScrollDirection } from 'src/hooks/useScrollDirection'
 
-import Headroom from 'react-headroom'
+// import Headroom from 'react-headroom'
 interface Props {
   children?: React.ReactNode
 }
@@ -14,13 +14,13 @@ export default function MainLayout({ children }: Props) {
 
   return (
     <div className='relative bg-white dark:bg-black'>
-      <div className='absolute h-full w-full'>
-        {/* <Headroom> */}
-        <Header />
-        {/* </Headroom> */}
-      </div>
-      {children}
-      {/* <div className='pt-10 md:pt-12 lg:pt-16'>{children}</div> */}
+      {/* <div className='absolute h-full w-full'> */}
+      {/* <Headroom> */}
+      <Header />
+      {/* </Headroom> */}
+      {/* </div> */}
+      {/* {children} */}
+      <div className='pt-10 md:pt-12 lg:pt-16'>{children}</div>
       <Footer />
     </div>
   )
